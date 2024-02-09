f20bd6a4535b577429ff779975882904f0e5eb98
tuyenvu1704 <tuyenvu1704@gmail.com>
2024-02-09 20:46:21 +0700

    boar-bar-full

--- src/pages/Board/BoardBar/index.jsx
+++ src/pages/Board/BoardBar/index.jsx
@@ -1,17 +1,153 @@
-import Box from "@mui/material/Box";
-
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import VpnLockIcon from '@mui/icons-material/VpnLock';
+import AddToDriveIcon from '@mui/icons-material/AddToDrive';
+import BoltIcon from '@mui/icons-material/Bolt';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import AvatarGroup from '@mui/material/AvatarGroup';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import Button from '@mui/material/Button';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+const MENU_STYLES = {
+  color: 'primary.main',
+  backgroundColor: 'white',
+  border: 'none',
+  px: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main',
+  },
+  '&:hover': {
+    bgcolor: 'primary.50',
+  },
+};
 function BoardBar() {
   return (
     <Box
+      px={2}
       sx={{
-        backgroundColor: "primary.dark",
-        width: "100%",
+        width: '100%',
         height: (theme) => theme.trello.boardBarHeight,
-        display: "flex",
-        alignItems: "center",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 2,
+        overflowX: 'auto',
+        borderTop: '1px solid #00bfa5',
       }}
     >
-      Board Bar
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 2,
+        }}
+      >
+        {/* MERN STACK */}
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label='MERN STACK'
+          variant='outlined'
+          clickable
+        />
+
+        {/* Public/Private Workspace */}
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label='Public/Private Workspace'
+          variant='outlined'
+          clickable
+        />
+
+        {/* Add to Google Drive */}
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label='Add to Google Drive'
+          variant='outlined'
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label='Automation'
+          variant='outlined'
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label='Filters'
+          variant='outlined'
+          clickable
+        />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 2,
+        }}
+      >
+        <Button
+          variant='outlined'
+          startIcon={<PersonAddIcon />}
+          sx={{ display: 'flex', alignItems: 'center' }}
+        >
+          Invite
+        </Button>
+        <AvatarGroup
+          max={7}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16,
+            },
+          }}
+        >
+          <Tooltip title='tuyen vu'>
+            <Avatar
+              alt='tuyen vu'
+              src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+            />
+          </Tooltip>
+          <Tooltip title='tuyen vu'>
+            <Avatar
+              alt='tuyen vu'
+              src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+            />
+          </Tooltip>
+          <Tooltip title='tuyen vu'>
+            <Avatar
+              alt='tuyen vu'
+              src='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+            />
+          </Tooltip>
+          <Tooltip title='tuyen vu'>
+            <Avatar
+              alt='tuyen vu'
+              src='https://images.unsplash.com/photo-1560250097-0b93528c311a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8fDA%3D'
+            />
+          </Tooltip>
+          <Tooltip title='tuyen vu'>
+            <Avatar
+              alt='tuyen vu'
+              src='https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHVzZXJ8ZW58MHx8MHx8fDA%3D'
+            />
+          </Tooltip>
+          <Tooltip title='tuyen vu'>
+            <Avatar
+              alt='tuyen vu'
+              src='https://images.unsplash.com/photo-1546961329-78bef0414d7c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHVzZXJ8ZW58MHx8MHx8fDA%3D'
+            />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   );
 }
